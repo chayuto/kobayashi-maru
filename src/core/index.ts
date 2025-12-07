48bac704365b234fb39accf3d4284cb13efc8393
Chayut Orapinpatipat <chayut@canopusnet.com>
2025-12-07 17:40:55 +1100

    refactor: Extract game input and state management into dedicated `GameInputHandler` and `GameStateController` modules.

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,6 +3,8 @@
  * Contains core game infrastructure and managers
  */
 export * from './Game';
+export * from './GameInputHandler';
+export * from './GameStateController';
 export * from './PerformanceMonitor';
 export * from './QualityManager';
 export * from './EventBus';
@@ -11,3 +13,4 @@ export * from './DebugManager';
 export * from './GestureManager';
 export * from './HapticManager';
 export * from './InputManager';
+
