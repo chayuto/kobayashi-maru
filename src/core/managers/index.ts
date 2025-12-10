2008d5c242c43268b99c0dabf6b1330354d3cdc8
Chayut Orapinpatipat <chayut@canopusnet.com>
2025-12-10 19:15:25 +1100

    refactor: relocate HUDData type export from UIController to ui/types.

--- src/core/managers/index.ts
+++ src/core/managers/index.ts
@@ -5,7 +5,8 @@ export { GameplayManager } from './GameplayManager';
 export type { GameplaySnapshot, GameplayCallbacks } from './GameplayManager';
 
 export { UIController } from './UIController';
-export type { UICallbacks, HUDData } from './UIController';
+export type { UICallbacks } from './UIController';
+export type { HUDData } from '../../ui/types';
 
 export { InputRouter, InputAction } from './InputRouter';
 export type { InputActionPayload, InputActionCallback, KeyBinding } from './InputRouter';
